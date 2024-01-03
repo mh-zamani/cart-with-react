e40bf40845b8b6f3f5242d54a6b59fcd1e6ade62
mahdiye <mahnixii98@gmail.com>
2024-01-04 00:46:50 +0330

    add all files

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Cart from "./component/cart"
+import Navbar from "./component/navbar"
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Cart/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/context.jsx
@@ -0,0 +1,48 @@
+import { useReducer } from "react";
+import { useContext } from "react";
+import { createContext } from "react";
+import { useEffect } from "react";
+import cartItems from "./data"
+import reducer from "./reducer";
+
+
+const CartContext = createContext();
+
+const initialState = {
+    loading : false,
+    cart: cartItems,
+    total: 0,
+    amount: 0,
+};
+
+const CartProvider = ({children}) =>{
+    const [state , dispatch] = useReducer(reducer , initialState);
+  
+    const ClearCart = () =>{
+        dispatch({type:"CLEAR_CART"});
+    };
+     
+    const remove = (id) =>{
+        dispatch({type: "REMOVE" , payload : id})
+    };
+
+    const changeQuantity = changeObj =>{
+   dispatch ({type :"CHANGE_QANTITY" , payload : changeObj})
+    };
+
+    useEffect( () => {
+     dispatch({type:"GET_TOTALS"});
+    },[state.cart]);
+
+    return(
+   <CartContext.Provider value={{...state , ClearCart , remove , changeQuantity}}>
+    {children} 
+    </CartContext.Provider>
+    );
+};
+
+
+const useCartContext = () =>{
+    return useContext(CartContext);
+};
+export {CartProvider , useCartContext}
